4c3d514941f4df89cb7a4b1357d67ed4baaafaff
张羽 <1147919561@qq.com>
2021-08-23 22:00:06 +0800

    Update config.php

--- www/wwwroot/35boke/config.php
+++ www/wwwroot/35boke/config.php
@@ -11,6 +11,6 @@
 	//下发参数:ordernumber=订单号码&gameid=分区ID&userid=充值账号&realmoney=实际充值金额&paychannel=充值通道&gamecurrency=游戏币&sign=MD5(充值通道密钥订单号码充值账号实际充值金额游戏币)
 	
 	// ip 账号 密码 账号库名
-	$db = array('127.0.0.1','root','gmduo.com','longwen','/data/sbin/logs/TLog/Tlog.800.0_'); 
+	$db = array('127.0.0.1','root','wool.com','longwen','/data/sbin/logs/TLog/Tlog.800.0_'); 
 	
 ?>
